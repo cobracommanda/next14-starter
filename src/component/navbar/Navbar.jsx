4c5ab8c946055de3875709e16cd8b45c80448b38
DRobinson <drobinson@benchmarkeducation.com>
2024-03-22 10:59:45 -0400

    add signIn and signOut social media

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import styles from "./navbar.module.css";
-import Links from "./links/Links"
+import Links from "./links/Links";
 import Link from "next/link";
+import { auth } from "@/lib/auth";
 
-const Navbar = () => { 
+const Navbar = async () => {
+  const session = await auth();
+  console.log(session);
   return (
     <div className={styles.container}>
-      <Link href="/" className={styles.logo}>Logo</Link>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
       <div>
-        <Links />
+        <Links session={session} />
       </div>
     </div>
   );
--- src/component/navbar/links/Links.jsx
+++ src/component/navbar/links/Links.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 import Image from "next/image";
+import { handleLogout } from "@/lib/actions";
+import { auth } from "@/lib/auth";
 const links = [
   {
     title: "Home",
@@ -21,10 +23,9 @@ const links = [
     path: "/blog",
   },
 ];
-const Links = () => {
+const Links = ({ session }) => {
   const [open, setOpen] = useState(false);
 
-  const session = true;
   const isAdmin = true;
 
   return (
@@ -33,10 +34,14 @@ const Links = () => {
         {links.map((link) => (
           <NavLink item={link} key={link.title} />
         ))}
-        {session ? (
+        {session?.user ? (
           <>
-            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-            <button className={styles.logout}>Logout</button>
+            {session.user?.isAdmin && (
+              <NavLink item={{ title: "Admin", path: "/admin" }} />
+            )}
+            <form action={handleLogout}>
+              <button className={styles.logout}>Logout</button>
+            </form>
           </>
         ) : (
           <NavLink item={{ title: "Login", path: "/login" }} />
