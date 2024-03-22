b4ffc9fc0b4ee56975de96dab2124ac42b1b8ce1
DRobinson <drobinson@benchmarkeducation.com>
2024-03-22 18:03:28 -0400

    add user with credentials

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,23 @@
+import { register } from "@/lib/actions";
+import styles from "./register.module.css";
 const RegisterPage = () => {
-  return <div>
-    <form action="">
-        <input type="text" placeholder="username" name="username" />
-    </form>
-  </div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <form className={styles.form} action={register}>
+          <input type="text" placeholder="username" name="username" />
+          <input type="email" placeholder="email" name="email" />
+          <input type="password" placeholder="password" name="password" />
+          <input
+            type="password"
+            placeholder="password again"
+            name="passwordRepeat"
+          />
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterPage;
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,8 +1,9 @@
 "use server";
 import { revalidatePath } from "next/cache";
-import { Post } from "./models";
+import { Post, User } from "./models";
 import { connectToDb } from "./utils";
 import { signIn, signOut } from "./auth";
+import bcrypt from "bcrypt";
 
 export const addPost = async (formData) => {
   const { title, desc, slug, userId } = Object.fromEntries(formData);
@@ -46,4 +47,39 @@ export const handleGithubLogin = async () => {
 export const handleLogout = async () => {
   "use server";
   await signOut();
+};
+
+export const register = async (formData) => {
+  const { username, email, password, img, passwordRepeat } =
+    Object.fromEntries(formData);
+
+  if (password !== passwordRepeat) {
+    return "Passwords do not match";
+  }
+
+  try {
+    connectToDb();
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return "Username already exist";
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      img,
+    });
+
+    await newUser.save();
+    console.log("saved to db");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
 };
