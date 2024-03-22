199134a6dadc78b45a2335746eb6ec9e49cb00ff
DRobinson <drobinson@benchmarkeducation.com>
2024-03-22 12:59:57 -0400

    timestamp 3:51:12

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return <div>
+    <form action="">
+        <input type="text" placeholder="username" name="username" />
+    </form>
+  </div>;
+};
+
+export default RegisterPage;
