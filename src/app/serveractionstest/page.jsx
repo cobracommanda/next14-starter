ebac5038a6412e5d67be50403db403a5eec4177a
DRobinson <drobinson@benchmarkeducation.com>
2024-03-21 21:15:00 -0400

    add server actions

--- src/app/serveractionstest/page.jsx
+++ src/app/serveractionstest/page.jsx
@@ -1,14 +1,22 @@
-import { sayHello } from '@/lib/actions'
-
+import { addPost, deletePost } from "@/lib/actions";
 
 const ServerActionPageTest = () => {
   return (
     <div>
-        <form action={sayHello}>
-            <button>Click Me</button>
-        </form>
-    </div>
-  )
-}
+      <form action={addPost}>
+        <input type="text" placeholder="title" name="title" />
+        <input type="text" placeholder="desc" name="desc" />
+        <input type="text" placeholder="slug" name="slug" />
+        <input type="text" placeholder="userId" name="userId" />
+        <button>Create</button>
+      </form>
 
-export default ServerActionPageTest
+      <form action={deletePost}>
+        <input type="text" placeholder="postId" name="postId" />
+        <button>Delete</button>
+      </form>
+    </div>
+  );
+};
+
+export default ServerActionPageTest;
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,4 +1,39 @@
-export const sayHello = async () => {
-  "use server";
-  console.log("hello");
+"use server";
+import { revalidatePath } from "next/cache";
+import { Post } from "./models";
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
+  const { title, desc, slug, userId } = Object.fromEntries(formData);
+  console.log(title, desc, slug, userId);
+
+  try {
+    connectToDb();
+    const newPost = new Post({
+      title,
+      desc,
+      slug,
+      userId,
+    });
+    await newPost.save();
+    console.log("saved to db");
+    revalidatePath("/blog");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
+};
+export const deletePost = async (formData) => {
+  const { id } = Object.fromEntries(formData);
+
+  try {
+    connectToDb();
+
+    await Post.findByIdAndDelete(id);
+    console.log("deleted from db");
+    revalidatePath("/blog");
+  } catch (err) {
+    console.log(err);
+    return { error: "Something went wrong!" };
+  }
 };
